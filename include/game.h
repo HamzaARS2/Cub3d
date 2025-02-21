fbf340d71dbdc2f34914dd7f094bd312154c6549
Naoufal <naoufal1337@gmail.com>
2025-02-21 02:25:29 +0100

    convert 2d map to 3d using perspective projection

--- include/game.h
+++ include/game.h
@@ -59,7 +59,7 @@ typedef struct s_game {
 } t_game;
 
 
-void	bresenham_line(t_game *game);
+void	bresenham_line(t_game *game, int *x, double angle);
 
 bool	init_game(t_game *game, char *mapfile);
 void	run_game(t_game *game);
